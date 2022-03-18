138d4b79ee3b72e3d34a92f8bbd1473b1b74b995
yongchongwu <yongchongwu@126.com>
2022-03-18 16:20:07 +0800

    feat(骨架搭建): 骨架搭建
    骨架搭建

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,43 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <h1>{{ msgStr }}</h1>
     <p>
       Start by not writing any code.
     </p>
+    <el-button @click="changeMsg">默认按钮</el-button>
   </div>
 </template>
 
 <script>
+import isArray from 'lodash/isArray';
+import dayjs from 'dayjs';
+
 export default {
     name: 'HelloWorld',
     props: {
         msg: String
+    },
+    data() {
+        return {
+            msgStr: this.msg
+        };
+    },
+    created() {
+        let me = this;
+        this.test();
+    },
+    watch: {
+        'msgStr': function (newVal, oldVal) {
+            console.log(newVal, oldVal);
+        }
+    },
+    methods: {
+        test() {
+            console.log(isArray([]));
+        },
+        changeMsg() {
+            this.msgStr = dayjs().format();
+        }
     }
 };
 </script>
